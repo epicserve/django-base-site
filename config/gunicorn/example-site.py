e532e8a352693e72d686ed83c933d915c0689fbd
Brent O'Connor <epicserve@gmail.com>
2012-01-26 14:06:53 -0800

    Updated gunicorn and nginx configs

--- config/gunicorn/example-site.py
+++
@@ -1,4 +0,0 @@
-pidfile = '/var/run/gunicorn_example-site.pid'
-proc_name = 'example-site'
-workers = 2
-bind = '0.0.0.0:8001'
---
+++ config/gunicorn/example_com.py
@@ -0,0 +1,4 @@
+pidfile = '/var/run/gunicorn_example.com.pid'
+proc_name = 'example.com'
+workers = 1
+bind = 'unix:/tmp/gunicorn_example.com.sock'
