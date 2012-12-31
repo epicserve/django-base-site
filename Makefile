9a4cecfc4737adb0d3588dca495c63c645d07d9b
Brent O'Connor <epicserve@gmail.com>
2012-12-31 11:58:58 -0800

    Fixed the create_local_settings_file.py script so it works

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 
 
 clean:
-	-./utils/create_local_settings_file.py
+	-python utils/create_local_settings_file.py
 	-rm utils/create_local_settings_file.py
 	-rm -rf docs/
 	-rm readme.rst
+	-rm .gitignore
 	-rm -rf static/js/site_name/
 	-rm -rf config/apache/
 	-rm -rf config/cron/
--- utils/create_local_settings_file.py
+++ utils/create_local_settings_file.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
-
 from django.utils.crypto import get_random_string
 from django.conf import settings
 import os
 
 LOCAL_DEV_TEMPLATE = """LOCAL_DEV_SETTINGS_LOADED = True
 
-SECRET_KEY = '{{ SECRET_KEY }}'
+SECRET_KEY = '<SECRET_KEY>'
 """
 
-SECRET_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-SECRET_KEY = get_random_string(50, SECRET_KEY_CHARS)
-LOCAL_DEV_TEMPLATE = LOCAL_DEV_TEMPLATE.replace('{{ SECRET_KEY }}', SECRET_KEY)
-TARGET_SETTINGS_FILE = os.path.join(settings.DJANGO_PROJECT_ROOT, 'config', 'settings', 'local_development.py')
+if __name__ == '__main__':
 
-if not os.path.exists(TARGET_SETTINGS_FILE):
-    with open(TARGET_SETTINGS_FILE, 'w') as f:
-        f.write(LOCAL_DEV_TEMPLATE)
-        f.close()
+    SECRET_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    SECRET_KEY = get_random_string(50, SECRET_KEY_CHARS)
+    LOCAL_DEV_TEMPLATE = LOCAL_DEV_TEMPLATE.replace('<SECRET_KEY>', SECRET_KEY)
+    TARGET_SETTINGS_FILE = os.path.join(settings.DJANGO_PROJECT_ROOT, 'config', 'settings', 'local_development.py')
+
+    if not os.path.exists(TARGET_SETTINGS_FILE):
+        with open(TARGET_SETTINGS_FILE, 'w') as f:
+            f.write(LOCAL_DEV_TEMPLATE)
+            f.close()
