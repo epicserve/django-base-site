4622ccafab99aa748037c4d7b861a91911e5368c
Brent O'Connor <epicserve@gmail.com>
2014-04-22 19:51:06 -0500

    Added support for compiling SASS
    - Updated all the site requirements
    - Installed bootstrap-sass
    - Update the settings based off the default Django 1.6 config template

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,111 +1,101 @@
+"""
+Django settings for {{ project_name }} project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-DJANGO_PROJECT_ROOT = os.path.abspath('%s/../../' % os.path.dirname(__file__))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '{{ secret_key }}'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'compressor',
 )
 
-MANAGERS = ADMINS
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                       # Or path to database file if using sqlite3.
-        'USER': '',                       # Not used with sqlite3.
-        'PASSWORD': '',                   # Not used with sqlite3.
-        'HOST': '',                       # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                       # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'config', 'db', 'db.sqlite3'),
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Vancouver'
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
+TIME_ZONE = 'UTC'
 
-SITE_NAME = 'Django Base Site'
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
-# Python module path where user will place custom format definition.
-# The directory where this setting is pointing should contain subdirectories
-# named as the locales, containing a formats.py file
-# (i.e. "myproject.locale" for myproject/locale/en/formats.py etc. use)
-FORMAT_MODULE_PATH = 'config.formats'
+USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(DJANGO_PROJECT_ROOT, 'media')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(DJANGO_PROJECT_ROOT, 'collected_static')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(DJANGO_PROJECT_ROOT, 'static'),
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+# SITE VARIABLES
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
+SITE_NAME = 'Django Base Site'
 
-ROOT_URLCONF = 'config.urls'
+
+# TEMPLATE SETTINGS
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
@@ -113,76 +103,21 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
-#    'django.core.context_processors.request',
+    'django.core.context_processors.tz',
+    # 'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
     'utils.context_processors.site_name',
 )
-
 TEMPLATE_DIRS = (
-    os.path.join(DJANGO_PROJECT_ROOT, 'templates', 'base'),
-    os.path.join(DJANGO_PROJECT_ROOT, 'templates'),
+    os.path.join(BASE_DIR, 'templates', 'base'),
+    os.path.join(BASE_DIR, 'templates'),
 )
 
-DJANGO_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'grappelli',
-    'django.contrib.admin',
-)
 
-THIRDPARTY_APPS = (
-    'compressor',
-    'south',
-)
+# DJANGO COMPRESSOR SETTINGS
 
-PROJECT_APPS = (
-
-)
-
-INSTALLED_APPS = DJANGO_APPS + THIRDPARTY_APPS + PROJECT_APPS
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-         }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
-# django_compressor settings
-# COMPRESS_ENABLED = True  # Seems to be needed in order for COMPRESS_PRECOMPILERS to work
 COMPRESS_OUTPUT_DIR = 'cache'
-# COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter', 'compressor.filters.cssmin.CSSMinFilter']
-# COMPRESS_PRECOMPILERS = (
-#     ('text/x-sass', 'sass {infile} {outfile}'),
-#     ('text/x-scss', 'sass --scss {infile} {outfile}'),
-# )
-
-# django-grappelli settings
-GRAPPELLI_ADMIN_HEADLINE = '%s Administration' % SITE_NAME
-GRAPPELLI_ADMIN_TITLE = '%s Administration' % SITE_NAME
+COMPRESS_PRECOMPILERS = (
+    ('text/x-sass', 'sass -l {infile} {outfile}'),
+    ('text/x-scss', 'sass --scss -l {infile} {outfile}'),
+)
---
+++ config/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import patterns, include
+from django.views.generic import TemplateView
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+
+handler500 = 'utils.views.server_error'
+
+urlpatterns = patterns('')
+
+# Debug/Development URLs
+if settings.DEBUG is True:
+    urlpatterns += patterns(
+        '',
+        (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    )
+
+# Admin Site
+admin.autodiscover()
+
+# Includes
+urlpatterns += patterns(
+    '',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^grappelli/', include('grappelli.urls')),
+)
+
+# Project Urls
+urlpatterns += patterns(
+    'django.views.generic',
+    (r'^$', TemplateView.as_view(template_name='index.html')),
+)
