747b79309a5fc131b64e0040917c4888d631eca3
Brent O'Connor <epicserve@gmail.com>
2014-09-03 22:09:47 -0500

    Updated the create_local_settings_file.py util

--- utils/create_local_settings_file.py
+++ utils/create_local_settings_file.py
@@ -2,7 +2,9 @@ from django.utils.crypto import get_random_string
 from django.conf import settings
 import os
 
-LOCAL_DEV_TEMPLATE = """LOCAL_DEV_SETTINGS_LOADED = True
+LOCAL_DEV_TEMPLATE = """from .dev import *
+
+LOCAL_SETTINGS_LOADED = True
 
 SECRET_KEY = '<SECRET_KEY>'
 """
@@ -12,7 +14,7 @@ if __name__ == '__main__':
     SECRET_KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
     SECRET_KEY = get_random_string(50, SECRET_KEY_CHARS)
     LOCAL_DEV_TEMPLATE = LOCAL_DEV_TEMPLATE.replace('<SECRET_KEY>', SECRET_KEY)
-    TARGET_SETTINGS_FILE = os.path.join(settings.DJANGO_PROJECT_ROOT, 'config', 'settings', 'local_development.py')
+    TARGET_SETTINGS_FILE = os.path.join(settings.BASE_DIR, 'config', 'settings', 'local.py')
 
     if not os.path.exists(TARGET_SETTINGS_FILE):
         with open(TARGET_SETTINGS_FILE, 'w') as f:
