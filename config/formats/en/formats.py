3e566b05807a472ce635798fedfd0d116a365a89
Collin Anderson <cmawebsite@gmail.com>
2014-09-08 15:43:40 -0400

    autopep8 -ir --ignore=E501

--- config/formats/en/formats.py
+++ config/formats/en/formats.py
@@ -11,12 +11,12 @@ YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'F j'
 SHORT_DATE_FORMAT = 'm/d/Y'
 SHORT_DATETIME_FORMAT = 'm/d/Y P'
-FIRST_DAY_OF_WEEK = 0 # Sunday
+FIRST_DAY_OF_WEEK = 0  # Sunday
 
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
 DATE_INPUT_FORMATS = (
-    '%m/%d/%Y', '%Y-%m-%d', '%m/%d/%y', # '10/25/2006', '2006-10-25', '10/25/06'
+    '%m/%d/%Y', '%Y-%m-%d', '%m/%d/%y',  # '10/25/2006', '2006-10-25', '10/25/06'
     # '%b %d %Y', '%b %d, %Y',            # 'Oct 25 2006', 'Oct 25, 2006'
     # '%d %b %Y', '%d %b, %Y',            # '25 Oct 2006', '25 Oct, 2006'
     # '%B %d %Y', '%B %d, %Y',            # 'October 25 2006', 'October 25, 2006'
--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -170,8 +171,8 @@ htmlhelp_basename = 'DjangoBaseSitedoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'DjangoBaseSite.tex', u'Django Base Site Documentation',
-   u'Brent O\'Connor', 'manual'),
+    ('index', 'DjangoBaseSite.tex', u'Django Base Site Documentation',
+     u'Brent O\'Connor', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
