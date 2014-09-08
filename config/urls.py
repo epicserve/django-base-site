2ebc293bd11d05b66d0ae7d8324ce69b524d182e
Collin Anderson <cmawebsite@gmail.com>
2014-09-08 15:38:22 -0400

    removed deprecated patterns()

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,25 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
 from django.conf import settings
 
 handler500 = 'utils.views.server_error'
 
-urlpatterns = patterns('')
+urlpatterns = []
 
 # Debug/Development URLs
 if settings.DEBUG is True:
-    urlpatterns += patterns(
-        '',
-        (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    )
+    urlpatterns += [
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    ]
 
 # Includes
-urlpatterns += patterns(
-    '',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^grappelli/', include('grappelli.urls')),
-)
+urlpatterns += [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
+]
 
 # Project Urls
-urlpatterns += patterns(
-    'django.views.generic',
-    (r'^$', TemplateView.as_view(template_name='index.html')),
-)
+urlpatterns += [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+]
