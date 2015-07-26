e404a692ce24ebddc07d50f43ecb6a2e6540cc7b
Brent O'Connor <epicserve@gmail.com>
2015-07-26 01:13:01 -0500

    Adding settings for heroku

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -6,7 +6,7 @@ DEBUG = False
 COMPRESS_ENABLED = False
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
