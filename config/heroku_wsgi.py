ce347491c7c6763f6ecfee80705f6972db23bf84
Brent O'Connor <epicserve@gmail.com>
2017-11-21 18:45:33 -0600

    Changed the DJANGO_SETTINGS_MODULE for heroku

--- config/heroku_wsgi.py
+++ config/heroku_wsgi.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.heroku")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = Cling(get_wsgi_application())
