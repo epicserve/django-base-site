91c7e42271c67052559ffe4f9ac900e2fae098e3
Brent O'Connor <epicserve@gmail.com>
2017-11-22 16:47:36 -0600

    :sparkles: Updated vagrant provisioning

--- config/vagrant/base.sh
+++ config/vagrant/base.sh
@@ -3,38 +3,66 @@ set -x
 
 cd /vagrant
 
+# VARIABLES
+export WORKON_HOME=/usr/local/virtualenvs
+export BASHRC='/home/vagrant/.bashrc'
+
 # INSTALL APT PACKAGES
-sudo aptitude -y update
-sudo aptitude -y upgrade
-sudo aptitude install -y git python-pip python-dev python3-dev libmysqlclient-dev htop
-sudo pip install pipenv
-sudo pipenv install --dev --python 3.4.3
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y git python3-pip python3-dev libmysqlclient-dev htop nodejs npm
+
+# SETUP PYTHON AND THE PROJECT VIRTUALENV
+pip3 install pipenv virtualenvwrapper
+sudo mkdir -p $WORKON_HOME
+sudo chown vagrant:vagrant $WORKON_HOME
+virtualenv --python $(which python3) "$WORKON_HOME/$PROJECT_NAME"
+. "$WORKON_HOME/$PROJECT_NAME/bin/activate"
+pipenv install --dev
+
+# SETUP NODE PACKAGES
+npm i -g gulp
+npm i
 
 # CUSTOMIZE THE PROFILE
-cat >> /home/vagrant/.bashrc << EOF
+if [ ! $(grep "WORKON_HOME" $BASHRC) ]; then
+
+cat >> $BASHRC << EOF
+
+export WORKON_HOME=$WORKON_HOME
+export PIP_REQUIRE_VIRTUALENV=true
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
 
 alias drs='/vagrant/manage.py runserver 0.0.0.0:8000'
 alias d='/vagrant/manage.py'
 complete -cf sudo
 
 cd /vagrant
+workon $PROJECT_NAME
 
 echo ""
 echo "Commands:"
-echo "drs - Start Django's runserver"
-echo "d   - Alias to Django's manage.py"
+echo "gulp   - Build SCSS and JS files"
+echo "workon - Select a virtualenv"
+echo "drs    - Start Django's runserver"
+echo "d      - Alias to Django's manage.py"
 echo ""
 EOF
+
+fi
 
 # SETUP THE LOCAL .env file
 if [ ! -f /vagrant/.env ]; then
 
-cat >> /vagrant/.env << EOF
-
+export SECRET_KEY=$(python -c "import random; print(''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)') for i in range(50)))")
+cat > .env <<EOF
 DEBUG=on
-SECRET_KEY='change-this-string-to-something-random'
-EMAIL_HOST_USER='epicserve@gmail.com'
-DEFAULT_FROM_EMAIL=$EMAIL_HOST_USER
+SECRET_KEY='$SECRET_KEY'
+EMAIL_HOST='smtp.planetspaceball.com'
+EMAIL_HOST_USER='skroob@planetspaceball.com'
+EMAIL_HOST_PASSWORD='12345'
+DEFAULT_FROM_EMAIL="President Skroob <skroob@planetspaceball.com>"
 EOF
 
 fi
