9a72d8bbfa1f3bd2bfce4c9116550bcb854e26a4
Brent O'Connor <epicserve@gmail.com>
2018-10-06 15:40:48 -0500

    Improved the start new project script

--- start_new_site.sh
+++ start_new_site.sh
@@ -47,12 +47,13 @@ get_project_name_slug
 get_project_directory
 
 curl -LOk $DOWNLOAD_LOCATION && unzip master
-mv django-base-site-master $PROJECT_DIRECTORY
+mkdir -p $PROJECT_DIRECTORY
+mv django-base-site-master/* $PROJECT_DIRECTORY/
+rm -rf django-base-site-master
 rm master.zip
 cd $PROJECT_DIRECTORY
-pipenv install --dev --python $(which python3)
-SECRET_KEY=$(python -c "import random; print(''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)') for i in range(50)))")
 
+SECRET_KEY=$(python -c "import random; print(''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)') for i in range(50)))")
 cat > .env <<EOF
 DEBUG=on
 SECRET_KEY='$SECRET_KEY'
@@ -62,28 +63,53 @@ EMAIL_HOST_PASSWORD='12345'
 DEFAULT_FROM_EMAIL="President Skroob <skroob@planetspaceball.com>"
 EOF
 
-pipenv run $PROJECT_DIRECTORY/manage.py migrate
-pipenv run $PROJECT_DIRECTORY/manage.py createsuperuser
-
-echo "
-To start the Django runserver in your new project:
-$ cd $PROJECT_DIRECTORY
-$ pipenv shell
-$ ./manage.py runserver
-
-If you don't plan on using Heroku for deployment, you can run 'make remove_heroku' to remove Heroku config files.
-"
-
-unset PROJECT_NAME_SLUG
-unset PROJECT_DIRECTORY
-
 make remove_extra_files
+USING_VAGRANT="no"
+USING_COMPOSE="yes"
 if [[ "no" = $(ask_yes_or_no_default_no "Are going to use Vagrant?") ]]; then
     make remove_vagrant
 else
-    echo "Warning: Vagrant is no longer supported and may or may not work.\n"
+    USING_VAGRANT="yes"
 fi
 
-if [[ "no" = $(ask_yes_or_no_default_yes "Are going to use Docker Compose?") ]]; then
+if [[ ${USING_VAGRANT} = "no" ]] && [[ "no" = $(ask_yes_or_no_default_yes "Are going to use Docker Compose?") ]]; then
     make remove_docker_compose
+    USING_COMPOSE="no"
+fi
+
+if [[ ${USING_VAGRANT} = "no" ]] && [[ ${USING_COMPOSE} = "no"  ]]; then
+
+    pipenv install --dev --python $(which python)
+    pipenv run $PROJECT_DIRECTORY/manage.py migrate
+    pipenv run $PROJECT_DIRECTORY/manage.py createsuperuser
+    echo ""
+    echo "Done."
+    echo ""
+    echo "To start the Django runserver in your new project:"
+    echo "$ cd $PROJECT_DIRECTORY"
+    echo "$ npm i"
+    echo "$ pipenv run ./manage.py runserver"
+
+elif [[ ${USING_VAGRANT} = "yes" ]]; then
+
+    echo ""
+    echo "Done."
+    echo ""
+    echo "WARNING: Vagrant is no longer supported and will eventually be removed."
+    echo ""
+    echo "To start Vagrant run:"
+    echo "$ cd $PROJECT_DIRECTORY"
+    echo "$ vagrant up"
+    echo ""
+
+elif [[ ${USING_COMPOSE} = "yes" ]]; then
+
+    echo ""
+    echo "Done."
+    echo ""
+    echo "To start Docker Compose run:"
+    echo "$ cd $PROJECT_DIRECTORY"
+    echo "$ docker-compose up"
+    echo ""
+
 fi
