9a31cbaa3f4e91c5c21ebdc188697e94c8f3e9ea
Brent O'Connor <epicserve@gmail.com>
2021-09-02 12:33:56 -0500

    Update settings so they work in a local venv

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,9 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 INTERNAL_IPS = env.list("INTERNAL_IPS", default=["127.0.0.1"])
 
 # Get the IP to use for Django Debug Toolbar when developing with docker
-ip = socket.gethostbyname(socket.gethostname())
-INTERNAL_IPS += [ip[:-1] + "1"]
+if env.bool("USE_DOCKER", default=False) is True:
+    ip = socket.gethostbyname(socket.gethostname())
+    INTERNAL_IPS += [ip[:-1] + "1"]
 
 # Application definition
 
