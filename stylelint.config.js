0f9f911e1f5fc2afcef9cf1cce192c9c9432fe13
Brent O'Connor <epicserve@gmail.com>
2022-10-29 12:13:31 -0500

    Fix stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,16 +2,9 @@ module.exports = {
   extends: [
     // Use the Standard config as the base
     // https://github.com/stylelint/stylelint-config-standard
-    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
     // Enforce a standard order for CSS properties
     // https://github.com/stormwarning/stylelint-config-recess-order
     'stylelint-config-recess-order',
   ],
-  plugins: [
-    // Bring in some extra rules for SCSS
-    'stylelint-scss',
-  ],
-  rules: {
-    'import-notation': 'string',
-  },
 };
