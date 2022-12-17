82b84aa80a3d1d6758b33436cc32cc06a21fbc7f
Brent O'Connor <epicserve@gmail.com>
2022-12-17 17:03:11 -0600

    Fix string length in migration

--- apps/accounts/migrations/0003_alter_user_groups_alter_user_is_active.py
+++ apps/accounts/migrations/0003_alter_user_groups_alter_user_is_active.py
@@ -16,7 +16,10 @@ class Migration(migrations.Migration):
             name="groups",
             field=models.ManyToManyField(
                 blank=True,
-                help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                help_text=(
+                    "The groups this user belongs to. A user will get all permissions granted to each of their "
+                    "groups."
+                ),
                 related_name="user_set",
                 related_query_name="user",
                 to="auth.group",
@@ -28,7 +31,10 @@ class Migration(migrations.Migration):
             name="is_active",
             field=models.BooleanField(
                 default=True,
-                help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                help_text=(
+                    "Designates whether this user should be treated as active. Unselect this instead of deleting "
+                    "accounts."
+                ),
                 verbose_name="active",
             ),
         ),
