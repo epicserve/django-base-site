5aaeeefc97bb45cf21af5abc5c3a591780e150b7
Brent O'Connor <epicserve@gmail.com>
2023-03-31 12:43:50 -0500

    Update Debuggy to run from manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,16 @@ import sys
 def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 5678))
+            print("Debugpy attached!")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
