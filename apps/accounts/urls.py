535c533742a6e5103203742fbcb921a16e96c377
Cody Fletcher <cody@codyfletcher.com>
2023-06-02 10:54:40 -0700

    Remove unused app/accounts/urls.py file

--- apps/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from apps.accounts.views import NameChange
-
-app_name = "accounts"
-urlpatterns = [
-    path("name/", NameChange.as_view(), name="account_change_name"),
-]
