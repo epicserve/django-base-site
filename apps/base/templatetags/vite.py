ee53bbbb1ef9f6c43b9be059329d7f3f9db709b2
Brent O'Connor <epicserve@gmail.com>
2023-12-26 11:35:26 -0600

    Add vite template tag coverage

--- apps/base/templatetags/vite.py
+++ apps/base/templatetags/vite.py
@@ -1,37 +1,64 @@
 import json
 from functools import lru_cache
+from typing import TypeVar
 
 from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
+T = TypeVar("T")
+
 register = template.Library()
 
-VITE_OUTPUT_DIR = getattr(settings, "VITE_OUTPUT_DIR", "dist/js/")
-VITE_MANIFEST_FILE = getattr(
-    settings, "VITE_MANIFEST_FILE", settings.STATIC_ROOT.joinpath(VITE_OUTPUT_DIR, ".vite", "manifest.json")
-)
-VITE_DEV_MODE = getattr(settings, "VITE_DEV_MODE", settings.DEBUG)
-VITE_SERVER_HOST = getattr(settings, "VITE_SERVER_HOST", "localhost")
-VITE_SERVER_PORT = getattr(settings, "VITE_SERVER_PORT", "3000")
+"""Object to allow settings to be overridden in tests"""
+
+
+class ViteSettings(object):
+    def _get_setting(self, var_name: str, default: T) -> T:
+        return getattr(settings, var_name, default)
+
+    @property
+    def VITE_DEV_MODE(self):
+        return self._get_setting("VITE_DEV_MODE", settings.DEBUG)
+
+    @property
+    def VITE_OUTPUT_DIR(self):
+        return self._get_setting("VITE_OUTPUT_DIR", "dist/js/")
+
+    @property
+    def VITE_MANIFEST_FILE(self):
+        return self._get_setting(
+            "VITE_MANIFEST_FILE", settings.STATIC_ROOT.joinpath(self.VITE_OUTPUT_DIR, ".vite", "manifest.json")
+        )
+
+    @property
+    def VITE_SERVER_HOST(self):
+        return self._get_setting("VITE_SERVER_HOST", "localhost")
+
+    @property
+    def VITE_SERVER_PORT(self):
+        return self._get_setting("VITE_SERVER_PORT", "3000")
+
+
+vite_settings = ViteSettings()
 
 
 def get_css_link(file: str) -> str:
-    base_url = f"{settings.STATIC_URL}{VITE_OUTPUT_DIR}"
+    base_url = f"{settings.STATIC_URL}{vite_settings.VITE_OUTPUT_DIR}"
     return mark_safe(f'<link rel="stylesheet" href="{base_url}{file}">')  # nosec B308, B703
 
 
 def get_script(file: str) -> str:
-    if VITE_DEV_MODE is False:
-        base_url = f"{settings.STATIC_URL}{VITE_OUTPUT_DIR}"
+    if vite_settings.VITE_DEV_MODE is False:
+        base_url = f"{settings.STATIC_URL}{vite_settings.VITE_OUTPUT_DIR}"
     else:
-        base_url = f"http://{VITE_SERVER_HOST}:{VITE_SERVER_PORT}{settings.STATIC_URL}"
+        base_url = f"http://{vite_settings.VITE_SERVER_HOST}:{vite_settings.VITE_SERVER_PORT}{settings.STATIC_URL}"
     return mark_safe(f'<script type="module" src="{base_url}{file}"></script>')  # nosec B308, B703
 
 
 @lru_cache
 def get_manifest():
-    with open(VITE_MANIFEST_FILE) as f:
+    with open(vite_settings.VITE_MANIFEST_FILE) as f:
         content = f.read()
         manifest = json.loads(content)
         return manifest
@@ -40,7 +67,7 @@ def get_manifest():
 @register.simple_tag
 def vite_asset(filename: str):
     is_css = str(filename).endswith("css")
-    if VITE_DEV_MODE is True:
+    if vite_settings.VITE_DEV_MODE is True:
         if is_css is True:
             return ""
         return get_script(filename)
@@ -53,21 +80,19 @@ def vite_asset(filename: str):
         file_data = manifest.get(filename)
 
     if file_data is None:
-        raise Exception(f'The vite asset "{filename}" was not found in the manifest file {VITE_MANIFEST_FILE}.')
+        raise Exception(
+            f'The vite asset "{filename}" was not found in the manifest file {vite_settings.VITE_MANIFEST_FILE}.'
+        )
 
     hashed_filename = file_data.get("css", [None])[0] if is_css is True else file_data.get("file")
 
     if is_css is True:
-        if hashed_filename is None:
-            raise Exception(
-                f'The vite asset "{hashed_filename}" was not found in the manifest file {VITE_MANIFEST_FILE}.'
-            )
         return get_css_link(hashed_filename)
     return get_script(hashed_filename)
 
 
 @register.simple_tag
-def vite_hmr_client():
-    if VITE_DEV_MODE is False:
+def vite_hmr_client() -> str:
+    if vite_settings.VITE_DEV_MODE is False:
         return ""
     return get_script("@vite/client")
---
+++ apps/base/tests/test_vite_templatetag.py
@@ -0,0 +1,100 @@
+from django.conf import settings
+from django.test import override_settings
+
+import pytest
+
+from apps.base.templatetags.vite import vite_asset, vite_hmr_client
+from apps.base.tests import BaseTest
+
+VITE_MANIFEST_FILE = settings.BASE_DIR / "apps" / "base" / "tests" / "vite_manifest.json"
+VITE_OUTPUT_DIR = "dist/"
+VITE_SERVER_HOST = "example.com"
+VITE_SERVER_PORT = "9999"
+STATIC_URL = "/static/"
+
+
+@override_settings(VITE_DEV_MODE=False, VITE_MANIFEST_FILE=settings.BASE_DIR / "vite_manifest.json")
+class TestViteAssetNoManifestFile(BaseTest):
+    def test_manifest_file_does_not_exist(self):
+        with pytest.raises(FileNotFoundError, match="No such file or directory"):
+            vite_asset("js/main.js")
+
+
+@override_settings(VITE_DEV_MODE=True, VITE_MANIFEST_FILE=VITE_MANIFEST_FILE)
+class TestViteAssetDevModeOn(BaseTest):
+    def test_js_asset(self):
+        result = vite_asset("js/main.js")
+        assert result == '<script type="module" src="http://localhost:3000/public/static/js/main.js"></script>'
+
+        with override_settings(
+            VITE_SERVER_HOST=VITE_SERVER_HOST, VITE_SERVER_PORT=VITE_SERVER_PORT, STATIC_URL=STATIC_URL
+        ):
+            result = vite_asset("js/main.js")
+            assert result == '<script type="module" src="http://example.com:9999/static/js/main.js"></script>'
+
+    def test_css_asset(self):
+        result = vite_asset("js/main.css")
+        assert result == ""
+
+
+@override_settings(VITE_DEV_MODE=False, VITE_MANIFEST_FILE=VITE_MANIFEST_FILE)
+class TestViteAssetDevModeOff(BaseTest):
+    def test_no_js_item(self):
+        with pytest.raises(
+            Exception,
+            match=f'The vite asset "js/does_not_exist.js" was not found in the manifest file {settings.BASE_DIR}/apps/base/tests/vite_manifest.json.',
+        ):
+            vite_asset("js/does_not_exist.js")
+
+    def test_no_css_item(self):
+        with pytest.raises(
+            Exception,
+            match=f'The vite asset "js/does_not_exist.css" was not found in the manifest file {settings.BASE_DIR}/apps/base/tests/vite_manifest.json.',
+        ):
+            vite_asset("js/does_not_exist.css")
+
+    def test_js_asset(self):
+        result = vite_asset("js/main.js")
+        assert result == '<script type="module" src="/public/static/dist/js/main-hashgoeshere.js"></script>'
+
+        with override_settings(STATIC_URL=STATIC_URL, VITE_OUTPUT_DIR=VITE_OUTPUT_DIR):
+            result = vite_asset("js/main.js")
+            assert result == '<script type="module" src="/static/dist/main-hashgoeshere.js"></script>'
+
+        with pytest.raises(
+            Exception,
+            match=f'The vite asset "js/does_not_exist.js" was not found in the manifest file {settings.BASE_DIR}/apps/base/tests/vite_manifest.json.',
+        ):
+            vite_asset("js/does_not_exist.js")
+
+    def test_css_asset(self):
+        result = vite_asset("js/main.css")
+        assert result == '<link rel="stylesheet" href="/public/static/dist/js/main-hashgoeshere.css">'
+
+        with override_settings(STATIC_URL=STATIC_URL, VITE_OUTPUT_DIR=VITE_OUTPUT_DIR):
+            result = vite_asset("js/main.css")
+            assert result == '<link rel="stylesheet" href="/static/dist/main-hashgoeshere.css">'
+
+        with pytest.raises(
+            Exception,
+            match=f'The vite asset "js/does_not_exist.css" was not found in the manifest file {settings.BASE_DIR}/apps/base/tests/vite_manifest.json.',
+        ):
+            vite_asset("js/does_not_exist.css")
+
+
+class TestViteHMRClientTagOn(BaseTest):
+    @override_settings(VITE_DEV_MODE=True)
+    def test_vite_hmr_client_dev_mode_on(self):
+        result = vite_hmr_client()
+        assert result == '<script type="module" src="http://localhost:3000/public/static/@vite/client"></script>'
+
+        with override_settings(
+            VITE_SERVER_HOST=VITE_SERVER_HOST, VITE_SERVER_PORT=VITE_SERVER_PORT, STATIC_URL=STATIC_URL
+        ):
+            result = vite_hmr_client()
+            assert result == '<script type="module" src="http://example.com:9999/static/@vite/client"></script>'
+
+    @override_settings(VITE_DEV_MODE=False)
+    def test_vite_hmr_client_dev_mode_off(self):
+        result = vite_hmr_client()
+        assert result == ""
