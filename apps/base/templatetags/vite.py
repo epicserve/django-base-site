2a05808feb6ac050a1d8c5f0f3af894318479743
Brent O'Connor <epicserve@gmail.com>
2023-12-26 12:31:07 -0600

    Refactor and clean up the Vite template tag

--- apps/base/templatetags/vite.py
+++ apps/base/templatetags/vite.py
@@ -10,10 +10,10 @@ T = TypeVar("T")
 
 register = template.Library()
 
-"""Object to allow settings to be overridden in tests"""
-
 
 class ViteSettings(object):
+    """Object to allow settings to be overridden in tests"""
+
     def _get_setting(self, var_name: str, default: T) -> T:
         return getattr(settings, var_name, default)
 
@@ -43,56 +43,63 @@ class ViteSettings(object):
 vite_settings = ViteSettings()
 
 
-def get_css_link(file: str) -> str:
+def _get_css_link(filename: str) -> str:
     base_url = f"{settings.STATIC_URL}{vite_settings.VITE_OUTPUT_DIR}"
-    return mark_safe(f'<link rel="stylesheet" href="{base_url}{file}">')  # nosec B308, B703
+    return mark_safe(f'<link rel="stylesheet" href="{base_url}{filename}">')  # nosec B308, B703
 
 
-def get_script(file: str) -> str:
+def _get_script_tag(filename: str) -> str:
     if vite_settings.VITE_DEV_MODE is False:
         base_url = f"{settings.STATIC_URL}{vite_settings.VITE_OUTPUT_DIR}"
     else:
         base_url = f"http://{vite_settings.VITE_SERVER_HOST}:{vite_settings.VITE_SERVER_PORT}{settings.STATIC_URL}"
-    return mark_safe(f'<script type="module" src="{base_url}{file}"></script>')  # nosec B308, B703
+    return mark_safe(f'<script type="module" src="{base_url}{filename}"></script>')  # nosec B308, B703
 
 
 @lru_cache
-def get_manifest():
+def _get_manifest():
     with open(vite_settings.VITE_MANIFEST_FILE) as f:
         content = f.read()
         manifest = json.loads(content)
         return manifest
 
 
-@register.simple_tag
-def vite_asset(filename: str):
-    is_css = str(filename).endswith("css")
-    if vite_settings.VITE_DEV_MODE is True:
-        if is_css is True:
-            return ""
-        return get_script(filename)
-
-    manifest = get_manifest()
-    if is_css is True:
-        js_filename = filename.replace(".css", ".js")
-        file_data = manifest.get(js_filename)
-    else:
-        file_data = manifest.get(filename)
-
+def _get_file_data(filename: str) -> dict[str, str | list[str | None] | bool]:
+    manifest = _get_manifest()
+    js_filename = filename.replace(".css", ".js") if filename.endswith(".css") else filename
+    file_data = manifest.get(js_filename)
     if file_data is None:
         raise Exception(
             f'The vite asset "{filename}" was not found in the manifest file {vite_settings.VITE_MANIFEST_FILE}.'
         )
+    return file_data
 
-    hashed_filename = file_data.get("css", [None])[0] if is_css is True else file_data.get("file")
 
-    if is_css is True:
-        return get_css_link(hashed_filename)
-    return get_script(hashed_filename)
+def _get_css_asset(filename: str):
+    if vite_settings.VITE_DEV_MODE is True:
+        return ""
+    file_data = _get_file_data(filename)
+    hashed_filename = file_data.get("css", [None])[0]  # type: ignore
+    return _get_css_link(hashed_filename)  # type: ignore
+
+
+def _get_js_asset(filename: str):
+    if vite_settings.VITE_DEV_MODE is True:
+        return _get_script_tag(filename)
+    file_data = _get_file_data(filename)
+    hashed_filename = file_data.get("file")
+    return _get_script_tag(hashed_filename)  # type: ignore
+
+
+@register.simple_tag
+def vite_asset(filename: str):
+    if str(filename).endswith("css") is True:
+        return _get_css_asset(filename)
+    return _get_js_asset(filename)
 
 
 @register.simple_tag
 def vite_hmr_client() -> str:
     if vite_settings.VITE_DEV_MODE is False:
         return ""
-    return get_script("@vite/client")
+    return _get_script_tag("@vite/client")
